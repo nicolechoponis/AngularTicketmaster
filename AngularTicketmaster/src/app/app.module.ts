301e21bfa7974bf579bbb0fd44f5537ab72a1b0d
Nicole Choponis <nicolechoponis@gmail.com>
2021-06-03 21:06:33 -0400

    added services and components

--- AngularTicketmaster/src/app/app.module.ts
+++ AngularTicketmaster/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { StarComponent } from './star/star.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { BucketlistPageComponent } from './bucketlist-page/bucketlist-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    StarComponent,
+    EventListComponent,
+    BucketlistPageComponent
   ],
   imports: [
     BrowserModule,
