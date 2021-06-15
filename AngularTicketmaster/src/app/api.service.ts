8018d02407e951956e8219076448bed20137a9a3
Nicole <nbush89@gmail.com>
2021-06-14 21:01:11 -0400

    added stars

--- AngularTicketmaster/src/app/api.service.ts
+++ AngularTicketmaster/src/app/api.service.ts
@@ -1,41 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { EventResponse,Event} from './interface';
-
+import { EventResponse, Event } from './interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class APIService {
-  apiURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=3s3A0oZm22CsOcaUT6JjbUXQlEb2EuyL";
-  Url = "https://app.ticketmaster.com/discovery/v2/events.json?";
-  api_key = "3s3A0oZm22CsOcaUT6JjbUXQlEb2EuyL";
-  eventDetailURL = "https://app.ticketmaster.com/discovery/v2/events/"
+  apiURL =
+    'https://app.ticketmaster.com/discovery/v2/events.json?apikey=3s3A0oZm22CsOcaUT6JjbUXQlEb2EuyL';
+  Url = 'https://app.ticketmaster.com/discovery/v2/events.json?';
+  api_key = '3s3A0oZm22CsOcaUT6JjbUXQlEb2EuyL';
+  eventDetailURL = 'https://app.ticketmaster.com/discovery/v2/events/';
   constructor(public client: HttpClient) {}
-  
-  getEvents(){
-    let pageURL=this.apiURL+"&page=1";
-    return this.client.get<EventResponse>(this.apiURL, {params:{page:1}});
-  }
-  getEventDetails(id:string){
-    let url = `${this.eventDetailURL}${id}.json?apikey=${this.api_key}`
-    return this.client.get<Event>(url)
 
-  //what is on line 13 for id is where we put the id that gets passed in 
+  getEvents() {
+    let pageURL = this.apiURL + '&page=1';
+    return this.client.get<EventResponse>(this.apiURL, { params: { page: 1 } });
   }
-  searchEvents(searchParameters: any){
+  getEventDetails(id: string) {
+    let url = `${this.eventDetailURL}${id}.json?apikey=${this.api_key}`;
+    return this.client.get<Event>(url);
+
+    //what is on line 13 for id is where we put the id that gets passed in
+  }
+  searchEvents(searchParameters: any) {
     let requestParameters: any = {
-    apikey: this.api_key 
+      apikey: this.api_key,
+    };
+    if (searchParameters.keyword.length > 0) {
+      requestParameters.keyword = searchParameters.keyword;
     }
-    if (searchParameters.keyword.length > 0){
-      requestParameters.keyword = searchParameters.keyword
+    if (searchParameters.city.length > 0) {
+      requestParameters.city = searchParameters.city;
     }
-    if (searchParameters.city.length > 0){
-      requestParameters.city = searchParameters.city
+    if (searchParameters.classificationName.length > 0) {
+      requestParameters.classificationName =
+        searchParameters.classificationName;
     }
-    if(searchParameters.classificationName.length > 0){
-      requestParameters.classificationName = searchParameters.classificationName
-    }
-    return this.client.get<EventResponse>(this.apiURL, {params: requestParameters});
+    return this.client.get<EventResponse>(this.apiURL, {
+      params: requestParameters,
+    });
   }
 }
--- AngularTicketmaster/src/app/star.service.ts
+++ AngularTicketmaster/src/app/star.service.ts
@@ -4,56 +4,49 @@ import { EventResponse, Event } from './interface';
 import { isNgTemplate } from '@angular/compiler';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StarService {
-
-  id: string ='';
+  id: string = '';
 
   isFavorite!: boolean;
   favoriteList: Event[] = [];
 
-  constructor(public client: HttpClient) { }
+  constructor(public client: HttpClient) {}
 
-//   foundFavorite(event: Event){
-//   if (event.isFavorite = true){
-//     button.addClass()
-//   }else {
-    
-//   }
-// }
+  //   foundFavorite(event: Event){
+  //   if (event.isFavorite = true){
+  //     button.addClass()
+  //   }else {
 
+  //   }
+  // }
 
-
-  toggleFavorite(event: Event){
-    let foundEvent = this.favoriteList.includes(event);
+  toggleFavorite(event: Event) {
+    let foundEvent = this.favoriteList.find((item) => event.id === item.id);
     // console.log(foundEvent);
-    if (foundEvent){
-      event.isFavorite=false;
-      let index = this.favoriteList.indexOf(event);
+    if (foundEvent) {
+      event.isFavorite = false;
+      let index = this.favoriteList.findIndex((item) => event.id === item.id);
       console.log(index);
       this.favoriteList.splice(index, 1);
-    }else {
-      event.isFavorite=true;
+    } else {
+      event.isFavorite = true;
       this.favoriteList.push(event);
     }
     console.log(this.favoriteList);
   }
 
-  getFavorites(){
+  getFavorites() {
     return this.favoriteList;
   }
 
   // not sure if this is working
-  onClick(){
+  onClick() {
     this.isFavorite = !this.isFavorite;
     console.log('Is favorite value: ' + this.isFavorite);
   }
 }
-
-
-
-
 
 // console.log(event.id);
 // this.favoriteList.push(event);
